c436b1a70bbb6b8dbade0f5e0588acd490a7ae24
yangjun <290968698@qq.com>
2020-02-12 22:33:53 +0800

    add go mod for Ymsg

--- build.sh
+++ build.sh
@@ -4,3 +4,8 @@
 mkdir -p dist
 pbjs --no-create --no-verify --no-convert --no-delimited --force-long   -t static-module -w es6 -o dist/index.js yrpcmsg.proto
 pbts -o dist/index.d.ts dist/index.js
+
+protoc --gogofaster_out=. yrpcmsg.proto
+rm yrpcmsg.pb.go
+mv github.com/yangjuncode/yrpcmsg/yrpcmsg.pb.go .
+rm -rf github.com
