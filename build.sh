d35b2050f2210228ef49d9ddeb81fb8e497b6f81
yangjun <290968698@qq.com>
2020-08-10 17:48:06 +0800

    switch to es6

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 mkdir -p dist
-pbjs --no-create --no-verify --no-convert --no-delimited --force-long   -t static-module -w commonjs -o dist/index.js yrpcmsg.proto
+pbjs --no-create --no-verify --no-convert --no-delimited --force-long   -t static-module -w es6 -o dist/index.js yrpcmsg.proto
 pbts -o dist/index.d.ts dist/index.js
 
 rm yrpcmsg.pb.go
